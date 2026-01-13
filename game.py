e79e73b9246b919071d6dd6cb3ab79eb3050aab6
AretoGoto <akiosoj@gmail.com>
2026-01-13 10:01:51 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,21 @@
+import random
+
+
+def computer_choice():
+    pass
+
+
+def user_choice():
+    pass
+
+
+def determine_winner(player_move, cpu_move):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
